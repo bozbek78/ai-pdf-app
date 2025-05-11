c963a1d6680feb27062abe5885a31b33c86c3328
bozbek78 <bozbek@gmail.com>
2025-05-11 23:15:18 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import gradio as gr
-from gradio_interface import build_interface
-
-gr_app = build_interface()
-
-if __name__ == "__main__":
-    gr_app.launch(server_name="0.0.0.0", server_port=7860, share=True)
