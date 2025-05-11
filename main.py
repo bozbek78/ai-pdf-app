196836888983158de9af62ad0cee0fc43f8f6930
bozbek78 <bozbek@gmail.com>
2025-05-11 23:15:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request, UploadFile, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import shutil
+import os
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload_pdf/")
+async def upload_pdf(request: Request, file: UploadFile):
+    file_location = f"uploads/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    return templates.TemplateResponse("index.html", {"request": request, "message": f"{file.filename} yüklendi."})
